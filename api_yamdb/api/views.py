5fcd53dcd991a563f1f4013e0e1f6b7c04807216
Илья Соболев <i4sobolev@yandex.ru>
2022-11-09 18:30:28 +0300

    first commit

---
+++ api_yamdb/api/views.py
@@ -0,0 +1,100 @@
+from django.shortcuts import get_object_or_404
+from django.db.models import Avg
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters, mixins, status, viewsets
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.decorators import api_view, action
+from rest_framework.permissions import (IsAuthenticated,
+                                        IsAuthenticatedOrReadOnly)
+from rest_framework.response import Response
+
+from reviews.models import Category, Genre, Title
+from api.paginator import CommentPagination
+from api.filters import TitleFilter
+from api.serializers import (CategoriesSerializer,
+                             CommentSerializer, GenresSerializer,
+                             ReviewSerializer,
+                             TitlesSerializer, TitlesViewSerializer)
+
+from rest_framework.pagination import LimitOffsetPagination
+
+from users.permissions import (IsAdminOrReadOnly,
+                               IsAdminModeratorAuthorOrReadOnly)
+
+
+class TitlesViewSet(viewsets.ModelViewSet):
+    queryset = Title.objects.annotate(rating=Avg('reviews__score'))
+    serializer_class = TitlesSerializer
+    permission_classes = [IsAdminOrReadOnly]
+    pagination_class = PageNumberPagination
+    filterset_class = TitleFilter
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = TitleFilter
+
+    def get_serializer_class(self):
+        if self.action in ['list', 'retrieve']:
+            return TitlesViewSerializer
+        return TitlesSerializer
+
+
+class ReviewGenreModelMixin(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.DestroyModelMixin,
+    viewsets.GenericViewSet
+):
+    permission_classes = [
+        IsAuthenticatedOrReadOnly,
+        IsAdminOrReadOnly
+    ]
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'slug')
+    lookup_field = 'slug'
+
+
+class CategoriesViewSet(ReviewGenreModelMixin):
+    queryset = Category.objects.all()
+    serializer_class = CategoriesSerializer
+
+
+class GenresViewSet(ReviewGenreModelMixin):
+    queryset = Genre.objects.all()
+    serializer_class = GenresSerializer
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    serializer_class = ReviewSerializer
+    pagination_class = CommentPagination
+    permission_classes = (IsAdminModeratorAuthorOrReadOnly,)
+
+    def get_queryset(self):
+        title = get_object_or_404(Title, id=self.kwargs.get('title_id'))
+        new_queryset = title.reviews.all()
+        return new_queryset
+
+    def perform_create(self, serializer):
+        title = get_object_or_404(Title, id=self.kwargs.get('title_id'))
+        serializer.save(author=self.request.user, title=title)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    pagination_class = CommentPagination
+    permission_classes = (IsAdminModeratorAuthorOrReadOnly,)
+
+    def get_queryset(self):
+        title = get_object_or_404(Title, id=self.kwargs.get('title_id'))
+        try:
+            review = title.reviews.get(id=self.kwargs.get('review_id'))
+        except TypeError:
+            TypeError('У произведения нет такого отзыва')
+        queryset = review.comments.all()
+        return queryset
+
+    def perform_create(self, serializer):
+        title = get_object_or_404(Title, id=self.kwargs.get('title_id'))
+        try:
+            review = title.reviews.get(id=self.kwargs.get('review_id'))
+        except TypeError:
+            TypeError('У произведения нет такого отзыва')
+        serializer.save(author=self.request.user, review=review)
